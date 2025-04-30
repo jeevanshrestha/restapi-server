33a6dc169bff8e5f98e2dfdbf3af1e056b47c320
jeevanshrestha <jeevanshrestha09@gmail.com>
2025-04-30 16:46:12 +1000

    add multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -4,27 +4,31 @@ import { Book } from "./bookTypes";
 
 
 export const create =(req : Request, res: Response, next: NextFunction) => {
+    //
+    const {} = req.body();
 
 }
 
 export const update =(req : Request, res: Response, next: NextFunction)=>{
 
+    const {} = req.body();
 
 }
 
 export const remove =(req : Request, res: Response, next: NextFunction)=> {
+    const {} = req.body();
 
 }
 
 export const edit =(req : Request, res: Response, next: NextFunction)=>{
-
+    const {} = req.body();
 }
 
 export const index =(req : Request, res: Response, next: NextFunction) =>{
-
+    const {} = req.body();
 }
 
 export const show =(req : Request, res: Response, next: NextFunction) => {
-
+    const {} = req.body();
 }
 
